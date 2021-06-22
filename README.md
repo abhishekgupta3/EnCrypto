4627a46b435715b2ac1889b64b6d6baa7dbfce2e
abhishekgupta3 <abhishek_1901ee03@iitp.ac.in>
2021-06-22 17:25:51 +0530

    update README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Getting Started with Create React App
+# EnCrypto 
+
+File based Encrytion and Decryption using AES Algorithm
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+-----------------------------------------------------------------------------------
 
 ## Available Scripts
 
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const ejs        =  require("ejs");
 const port       =  process.env.PORT || 8000;
 const bodyParser =  require("body-parser");
 // const multer     =  require('multer'); 
-const path       =  require("path");
-const fs         =  require('fs');
 const CryptoJS   =  require("crypto-js");
 
 // default options
@@ -13,7 +11,7 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+// Get routes
 app.get('/',(req,res)=>{
 	res.render('home');
 });
@@ -28,64 +26,6 @@ app.get('/encrypt',(req,res)=>{
 app.get('/decrypt',(req,res)=>{
 	res.render('decrypt');
 });
-
-// app.post("/FileUpload",function (req, res) {
-//     console.log(req.body)
-//     res.render('encrypt');
-// });
-
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         // Uploads is the Upload_folder_name
-//         cb(null, "uploads")
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + "-" + Date.now()+".pdf")
-//     }
-//  })
-
-// // maximum size for uploading
-// const maxSize = 1 * 1000 * 1000;
-    
-// const upload = multer({ 
-//     storage: storage,
-//     limits: { fileSize: maxSize },
-//     fileFilter: function (req, file, cb){
-    
-//         // Set the filetypes, it is optional
-//         const filetypes = /pdf/;
-//         const mimetype = filetypes.test(file.mimetype);
-  
-//         const extname = filetypes.test(path.extname(
-//                     file.originalname).toLowerCase());
-        
-//         if (mimetype && extname) {
-//             return cb(null, true);
-//         }
-      
-//         cb("Error: File upload only supports ' .pdf ' filetypes ");
-//       } 
-  
-// }).single("myfile");     
-
-// app.post("/FileUpload",function (req, res, next) {
-        
-//     // Error MiddleWare for multer file upload, so if any error occurs, the file would not be uploaded!
-//     upload(req,res,function(err) {
-  
-//         if(err) {
-//             // ERROR occured (due to uploading file of size 
-//             // greater than 1MB or uploading different file type)
-//             res.send(err)
-//         }
-//         else {
-//             // SUCCESS, file successfully uploaded
-//             res.send("Success, File uploaded!")
-//         }
-//     })
-// })
-
 
 app.listen(port, function(){
     console.log("Server listing at port...");
