5fb21f427e3e49b274469541c559bd092cab1fb1
abhishekgupta3 <abhishek_1901ee03@iitp.ac.in>
2024-06-15 23:42:40 +0530

    change folder structure, remove ejs

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const express    =  require('express');
-const app        =  express();
-const ejs        =  require("ejs");
-const port       =  process.env.PORT || 8000;
-const bodyParser =  require("body-parser");
-const CryptoJS   =  require("crypto-js");
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+const CryptoJS = require("crypto-js");
 
 // default options
-app.set("view engine","ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-// res.header('Content-Security-Policy', "img-src 'self'");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Get routes
-app.get('/',(req,res)=>{
-	res.render('home');
-});
-app.get('/about',(req,res)=>{
-	res.render('about');
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
-app.get('/encrypt',(req,res)=>{
-	res.render('encrypt');
+app.get('/encrypt', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'encrypt.html'));
 });
 
-app.get('/decrypt',(req,res)=>{
-	res.render('decrypt');
+app.get('/decrypt', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'decrypt.html'));
 });
 
-app.listen(port, function(){
+app.listen(port, function () {
+	console.log(`Server is running on http://localhost:${port}`);
 });
